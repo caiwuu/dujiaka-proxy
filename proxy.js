e17f6512dfac5d69298b64f07bbbd2465a8c9cd4
蔡武 <2252711582@qq.com>
2024-07-13 18:26:51 +0800

    1

--- proxy.js
+++ proxy.js
@@ -1,29 +1,37 @@
-var https = require('https');
-var fs = require('fs');
-var httpProxy = require('http-proxy');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-// 读取证书和私钥
-var options = {
-  key: fs.readFileSync('privkey.pem'),
-  cert: fs.readFileSync('fullchain.pem')
-};
-
-var proxy = httpProxy.createProxyServer({
+// 创建一个代理服务器实例
+const proxy = httpProxy.createProxyServer({
   target: process.env.proxy_target,
-  secure: false // 如果目标服务器使用自签名证书，需要设置为false
+  secure: false, // 如果目标服务器使用的是自签名证书，可以将此选项设置为false
+  changeOrigin: true // 更改origin头
 });
 
-proxy.on('error', function (err, req, res) {
+// 创建一个http服务器
+const server = http.createServer((req, res) => {
+  // 在接收到请求时，将请求代理到目标服务器
+  proxy.web(req, res, (err) => {
+    if (err) {
+      console.error('Proxy error:', err);
+      res.writeHead(500, {
+        'Content-Type': 'text/plain'
+      });
+      res.end('Something went wrong.');
+    }
+  });
+});
+
+// 启动服务器，监听指定端口
+server.listen(8080, () => {
+  console.log('Proxy server is running on http://localhost:3000');
+});
+
+// 处理代理服务器错误
+proxy.on('error', (err, req, res) => {
+  console.error('Proxy error:', err);
   res.writeHead(500, {
     'Content-Type': 'text/plain'
   });
   res.end('Something went wrong.');
-});
-
-var proxy_server = https.createServer(options, function (req, res) {
-  proxy.web(req, res);
-});
-
-proxy_server.listen(8080, function () {
-  console.log('proxy server is running on port 8080');
 });
